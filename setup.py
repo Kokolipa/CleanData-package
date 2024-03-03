e2c8934392c491dbe72a44cdd4b97481573d62fd
Kokolipa <galbeeri1@gmail.com>
2024-03-03 15:04:11 +1100

    Modifying setup.py - adding my github link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(where="CleanData"),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ArjanCodes/2023-package", #TODO: Create the GitHub repor
+    url="https://github.com/Kokolipa/CleanData-package/tree/CleanData_main", 
     author="Kokolipa",
     author_email="galbeeri1@gmail.com",
     license="MIT", 
