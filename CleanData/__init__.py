b0d3fc1fa340332db2ef95f613b0b150124e6e21
Kokolipa <galbeeri1@gmail.com>
2024-03-04 10:14:16 +1100

    Adding the QA Python file. This file contain the 'Ask' function that implements TAPAS MLM(Table-based Pretraining and Answer Selection) model created by Google AI.

--- CleanData/__init__.py
+++ CleanData/__init__.py
@@ -2,5 +2,6 @@
 from .anomalies import Anomalies  # noqa: F401
 from .find_treat_duplicates import FindTreatDuplicates  # noqa: F401
 from .memory import Memory  # noqa: F401
+from .qa import QA  # noqa: F401
 from .text_typos import TextTypos  # noqa: F401
 from .treat_na import TreatNA  # noqa: F401
---
+++ CleanData/qa.py
@@ -0,0 +1,48 @@
+import pandas as pd  # noqa: F401
+from transformers import (AutoModelForTableQuestionAnswering,  # noqa: F401
+                          AutoTokenizer, pipeline)
+
+
+class QA:
+    # Load model & tokenizer
+    model = 'google/tapas-base-finetuned-wtq'
+    tapas_model = AutoModelForTableQuestionAnswering.from_pretrained(model)
+    tapas_tokenizer = AutoTokenizer.from_pretrained(model)
+    
+    # Initializing pipeline
+    nlp = pipeline('table-question-answering', model=tapas_model, tokenizer=tapas_tokenizer)
+
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def Ask(cls, query: str, data: pd.DataFrame):
+        """
+            Asks a natural language question about a given pandas DataFrame and prints the answer.
+
+            This method utilizes a pre-trained TAPAS model for table-based question answering to interpret the data
+            in the DataFrame and provide answers to questions posed in natural language. It's designed to work with
+            data that can be represented in tabular form and can handle a wide range of question types, including
+            aggregations, comparisons, and single-row lookups.
+
+            Example usage:
+                Ask('Which city has the highest download speed?', data)
+
+            Args:
+                query (str): The natural language question to be asked about the data.
+                data (pd.DataFrame): The pandas DataFrame containing the data to be queried. 
+                
+            Returns:
+                None. The answer is printed out. 
+        """
+
+        # Applying a string type for data
+        data = data.astype(str)
+        
+        print(query)
+        print('>>>>>')
+        result = cls.nlp({'table': data, 'query': query})
+        answer = result['cells']
+        print(answer)
+
+        
